885c77ccef4a15d69829b8f8abb1b6aba3c4ebe6
niru9711 <nirusanant97@gmail.com>
2024-11-21 22:53:35 +0530

    dynamic_content

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render, redirect
 from  django.http import HttpResponse
+import logging
 
-
-def index(request):
-    blog_title = "Latest Posts"
-    posts = [
+posts = [
         {'id':1, 'title': 'post1', 'content': 'content of the post 1'},
         {'id':2,'title': 'post2', 'content': 'content of the post 2'},
         {'id':3,'title': 'post3', 'content': 'content of the post 3'},
         {'id':4,'title': 'post4', 'content': 'content of the post 4'},
     ]
+
+def index(request):
+    blog_title = "Latest Posts"    
     return render(request, 'blog/index.html', {'blog_title': blog_title, 'posts': posts})
 
 def detail(request, post_id):
-    return render(request, 'blog/detail.html')
+    post = next((item for item in posts if item['id']==post_id), None)
+    logger = logging.getLogger('TESTING')
+    logger.debug(f"post is available {post}")
+    return render(request, 'blog/detail.html', {'post':post, 'posts':posts})
 
 def old_url_redirect(requset):
     return redirect("blog:new_url")
