b20bbce45ba883987335ea9ee15a87943fcc6f46
niru9711 <nirusanant97@gmail.com>
2024-11-22 23:09:54 +0530

    get_data_from_model

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render, redirect
 from  django.http import HttpResponse
 import logging
+from .models import Post
 
-posts = [
-        {'id':1, 'title': 'post1', 'content': 'content of the post 1'},
-        {'id':2,'title': 'post2', 'content': 'content of the post 2'},
-        {'id':3,'title': 'post3', 'content': 'content of the post 3'},
-        {'id':4,'title': 'post4', 'content': 'content of the post 4'},
-    ]
+## static data
+# posts = [
+#         {'id':1, 'title': 'post1', 'content': 'content of the post 1'},
+#         {'id':2,'title': 'post2', 'content': 'content of the post 2'},
+#         {'id':3,'title': 'post3', 'content': 'content of the post 3'},
+#         {'id':4,'title': 'post4', 'content': 'content of the post 4'},
+#     ]
 
 def index(request):
+    posts = Post.objects.all()
     blog_title = "Latest Posts"    
     return render(request, 'blog/index.html', {'blog_title': blog_title, 'posts': posts})
 
 def detail(request, post_id):
-    post = next((item for item in posts if item['id']==post_id), None)
+    # post = next((item for item in posts if item['id']==post_id), None)   # static data
+    
+    posts = Post.objects.all()
+    post = Post.objects.get(pk=post_id)
     # logger = logging.getLogger('TESTING')
     # logger.debug(f"post is available {post}")
     return render(request, 'blog/detail.html', {'post':post, 'posts':posts})
