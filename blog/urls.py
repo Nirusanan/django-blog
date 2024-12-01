1af182fc7b60835e4523584c9693134580832df2
niru9711 <nirusanant97@gmail.com>
2024-12-01 13:56:15 +0530

    login_signup

--- blog/urls.py
+++ blog/urls.py
@@ -3,12 +3,13 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.login, name="login"),
+    path("blog", views.index, name="index"),
     path("post/<str:slug>", views.detail, name="detail"),
     path("old_url", views.old_url_redirect, name="old_url"),
     path("new_url", views.new_url_view, name="new_url"),
     path("contact", views.contact_view, name="contact"),
     path("aboutus", views.aboutus_view, name="about"),
-
-
+    path("signup", views.signup, name="signup"),
+    path('signout', views.signout, name='signout'),
 ]
